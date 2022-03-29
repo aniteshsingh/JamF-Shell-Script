14e71820e3a919498d2e9227d071a0ea5e987613
Anitesh Singh <anitesh.iete@gmail.com>
2022-03-29 20:04:05 +0530

    shell script to delete local user from mac

---
+++ rm_user.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+
+localUsers=($(dscl . list /Users UniqueID | grep -v "_" | awk '$2 > 500 && $2 < 1000 {print $1}'))
+
+for usr in ${localUsers[@]}; do
+	if [[ $usr = "taskus" ]]; then
+		echo "Found user $usr . Deleting now ..."
+		sudo dscl . -delete /Users/$usr
+		sudo rm -rf /Users/$usr
+	fi
+done
