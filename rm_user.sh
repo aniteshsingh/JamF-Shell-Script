b71a855f10d18416b0056018701f61d927b52c5c
Anitesh Singh <anitesh.iete@gmail.com>
2022-03-29 20:16:13 +0530

    changed username to be deleted

--- rm_user.sh
+++ rm_user.sh
@@ -4,7 +4,7 @@
 localUsers=($(dscl . list /Users UniqueID | grep -v "_" | awk '$2 > 500 && $2 < 1000 {print $1}'))
 
 for usr in ${localUsers[@]}; do
-	if [[ $usr = "taskus" ]]; then
+	if [[ $usr = "user_name" ]]; then
 		echo "Found user $usr . Deleting now ..."
 		sudo dscl . -delete /Users/$usr
 		sudo rm -rf /Users/$usr
